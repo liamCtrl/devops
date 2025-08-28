e4c89df4e0a9e6f2635c0ef751312c920ec4b6e1
liamCtrl <122392454+liamCtrl@users.noreply.github.com>
2025-08-28 14:28:23 +0000

    Add a feature to detect if n is a negative number

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 exports.fibonacci = function(n) {
-    if(n == 0 || n == 1)
+     if(n < 0)
+        return "undefined";
+     if(n == 0 || n == 1)
         return n;
     return this.fibonacci(n-1) + this.fibonacci(n-2);
 }
